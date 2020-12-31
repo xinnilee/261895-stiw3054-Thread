84c5ac535c44ce5a42d088d1c445a8b58ea7599f
xinnilee <48663953+xinnilee@users.noreply.github.com>
2020-12-31 09:22:37 +0800

    Initial commit

---
+++ src/main/java/com/xinni/HelloThread.java
@@ -0,0 +1,22 @@
+package com.xinni;
+
+public class HelloThread  extends Thread{
+
+
+    @Override
+    public void run() {
+        System.out.println("Hello Thread " + Thread.currentThread().getName());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getId());
+
+        HelloThread helloThread1= new HelloThread();
+        HelloThread helloThread2= new HelloThread();
+        helloThread1.start();
+        helloThread2.start();
+
+    }
+
+}
